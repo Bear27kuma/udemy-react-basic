fdf8961718feb9d7c00f271e7fe64973c678198d
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-01 00:02:45 +0900

    イベントやstyleの扱い方

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -3,12 +3,23 @@ import React from 'react';
 // コンポーネント名は必ず先頭を大文字から始める
 // 複数単語の命名はパスカルケースで行う（ex. SomeComponent）
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: 'blue',
+    // CSSのプロパティもキャメルケースで記述する
+    fontSize: '18px'
+  };
   return (
     // JSX記法は一つのタグで囲んだ内容しかレンダリングできない
     // React.Fragmentで囲うことで不要なHTMLタグをレンダリングしない（<React.Fragment>と<>は同じ意味）
     <>
-      <h1>こんにちは!</h1>
-      <p>お元気ですか？</p>
+      {/*styleはJSのオブジェクトとしてセットすることができる*/}
+      <h1 style={{ color: 'red' }}>こんにちは!</h1>
+      {/*別で定義したオブジェクトをstyleに当てることもできる*/}
+      <p style={contentStyle}>お元気ですか？</p>
+      {/*JSX記法の場合、HTMLの属性はキャメルケースで記述する*/}
+      {/*JSXでは波括弧はJSの記述の意味*/}
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
