51c4c6343e3fe257cefafcebd7f3d24e579603c4
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-01 22:30:59 +0900

    CSSでスタリングをする

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './styles.css';
 
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力"/>
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
           <li>
             <p>TODOです</p>
@@ -19,13 +20,13 @@ export const App = () => {
         <ul>
           <li>
             <p>TODOです</p>
-            <button>完了</button>
+            <  button>完了</button>
             <button>削除</button>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
           <li>
             <p>TODOです</p>
